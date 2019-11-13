362bfd705d44809b6075312a59012e7ee9ed528f
Romario Gustave <romagustave10@gmail.com>
2019-11-13 08:55:58 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+def badge_maker(name)
+  puts ("Enter your name")
+  name=name.chomp
+  return " hello my name is #{name}"
+end 
+def batch_badge_creator(array)
+  array = ["Edsger, Ada, Charles, Alan, Grace, Linus, Matz"]
+  array.each do |array|
+    return "#{array}"
+  end
+  def assign_rooms(list)
+    room = [1..7]
+    list = ["Edsger, Ada, Charles, Alan, Grace, Linus, Matz"]
+    while counter = 0 
+    
